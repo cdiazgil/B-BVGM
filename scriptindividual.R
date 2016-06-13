c9974191f16b1932c780a65940a45a368618ae11
cdiazgil <diazgil.carlos@gmail.com>
2016-06-13 14:41:04 +0200

    Add files via upload
    Ok many changes in terms of implementing the new individual model and making it work properly

--- scriptindividual.R
+++ scriptindividual.R
@@ -1,167 +1,27 @@
 
 f2 <- function(data){
-# Now is time for the Individual models
+  # Now is time for the Individual models
   library(R2jags) 
   
-#-------------
-# Section 3: BAYESIAN ESTIMATION OF VON BERTALANFY PARAMETERS
-# INDIVIDUAL MODEL Several observations per fish
-#
-#-------------
-#STEP 1: INITIALS
-#-------------
-#getwd()
-#data<-read.csv(file = "example.csv",sep=";")
-
-#names(data)
-ID=NULL
-for (i in 1: length(unique(data$fishID))){
-  temp2<-rep(i,length(data$age[which(data$fishID==unique(data$fishID)[i])]))
+  #-------------
+  # Section 3: BAYESIAN ESTIMATION OF VON BERTALANFY PARAMETERS
+  # INDIVIDUAL MODEL Several observations per fish
+  #
+  #-------------
+  #STEP 1: INITIALS
+  #-------------
+  #getwd()
+  #data<-read.csv(file = "www/Scorpaena.csv",sep=";")
   
-  ID=c(ID,temp2)
-}
-data$ID=ID
-
-
-#maximum age of each fish== later will be AGE
-Age=NULL
-for (i in 1: length(unique(data$fishID))){
-  temp<-max(data$age[which(data$fishID==unique(data$fishID)[i])])
-  Age=c(Age,temp)
-}
-maxage=max(Age)
-
-nfish=length(unique(data$fishID))# Sample size
-inits <- function() list(
-  Linf=runif(nfish,2,10),
-  k=runif(nfish,0,1),
-  T0=runif(nfish,-1,1),
-  tau=1,
-  tauk=1,
-  tauLinf=1,
-  tauT0=1,
-  k.beta=0,
-  Linf.beta=0
-)
-
-#-------------
-#STEP 2:DATA
-#-------------
-
-
-# Prepare the data for JAGS
-jags.data <- list(
-  nobs=length(data$age),
-  nfish=nfish,
-  age=data$age,
-  size=data$size,
-  ID=data$ID,
-  AGE=Age,
-  MaxAge=max(Age)
-)
-
-#-------------
-#STEP 3: INDIVIDUAL BAYESIAN MODEL
-#-------------
-
-sink("individualmodel.txt")
-cat("
-    model {
-    for( i in 1 : nobs ) {
-    mu[i]<-Linf[ID[i]]*(1-exp(-k[ID[i]]*(age[i]-T0[ID[i]])))
-    size[i] ~ dnorm(mu[i],tau)
-    }
-    #priors individual fish 
-    for( i in 1 : nfish ) {
-    T0[i]~dnorm(T0.alpha,tauT0)
-    Linf[i]~dnorm(Linf.exp[i],tauLinf)T(2,10)
-    Linf.exp[i]<-Linf.alpha+Linf.beta*AGE[i]
-    k[i]~dnorm(k.exp[i],tauk)T(0,1)
-    k.exp[i]<-k.alpha+k.beta*AGE[i]
-    }
-    T0.alpha~dnorm(0,0.000001)
-    Linf.alpha~dnorm(0,0.000001)
-    k.alpha~dnorm(0,0.000001)
-    Linf.beta~dnorm(0,0.000001)T(-1,1)
-    k.beta~dnorm(0,0.000001)T(-1,1)
+  #names(data)
+  ID=NULL
+  for (i in 1: length(unique(data$fishID))){
+    temp2<-rep(i,length(data$age[which(data$fishID==unique(data$fishID)[i])]))
     
-    #prior variances
-    tau ~ dgamma(0.001,0.001)
-    tauT0 ~ dgamma(0.001,0.001)
-    tauk ~ dgamma(0.001,0.001)
-    tauLinf ~ dgamma(0.001,0.001)
-    
-    #estimating size at age for each grup
-    for(i in 1 : MaxAge) {
-    estimated[i,1]<-0
-    }
-    for(j in 2 : MaxAge){
-    Linf.hat[j]<-Linf.alpha+Linf.beta*j
-    k.hat[j]<-k.alpha+k.beta*j
-    T0.hat[j]<-T0.alpha#+T0.beta*j
-    for(i in 1 : MaxAge) {
-    estimated[i,j]<-(Linf.hat[j])*(1-exp(-(k.hat[j])*(i-(T0.hat[j]))))
-    }
-    }
-    }
-    ",fill = TRUE)
-sink()
-
-#-------------
-#STEP 4: RUNNING THE MODEL AND UPDATING
-#-------------
-
-# Parameters monitored
-params =c("T0","Linf","k","Linf.alpha","Linf.beta","k.alpha","k.beta",
-          "T0.alpha","estimated")
-
-# MCMC settings
-nt <- 5
-ni <- 1000
-nb <- 100
-nc <- 3
-
-out <- jags(jags.data, inits, params, "individualmodel.txt", 
-            n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb)
-
-# Since this is calculating one curve per fish it is really time consuming and 
-# only a few traceplots are usefull to see if the Markov Chains have converged
-
-#traceplot(out,varname=c("deviance"))
-#traceplot(out,varname=c("Linf.alpha"))
-#traceplot(out,varname=c("Linf.beta"))
-#traceplot(out,varname=c("k.alpha"))
-#traceplot(out,varname=c("k.beta"))
-
-# A rough estimation of how long the updating process will take 
-# is provided here. 
-pre.time <- Sys.time()
-out <- update(out, n.iter=5000,n.thin=10)
-post.time <- Sys.time()
-post.time-pre.time
-
-### WARNING VERY TIME CONSUMING (HOURS)
-# out <- update(out, n.iter=500000,n.thin=100)
-
-
-#-------------
-#STEP 5: RESULTS VISUALIZATION 
-#-------------
-
-# To show the results and the estimates of the von Bertalanffy parameters
-print(out)
-# Or
-out$BUGSoutput$summary
-
-}
-# Plot to see the results
-
-f4 <- function(indparams,data){
+    ID=c(ID,temp2)
+  }
+  data$ID=ID
   
-  pepe=indparams
- #pepe= out$BUGSoutput$summary
-  
- #data<-read.csv(file = "data.csv",sep=";")
   
   #maximum age of each fish== later will be AGE
   Age=NULL
@@ -169,42 +29,170 @@ f4 <- function(indparams,data){
     temp<-max(data$age[which(data$fishID==unique(data$fishID)[i])])
     Age=c(Age,temp)
   }
+  maxage=max(Age)
+  
+  nfish=length(unique(data$fishID))# Sample size
+  inits <- function() list(
+    Linf=runif(nfish,1,10),
+    k=runif(nfish,0,1),
+    T0=runif(nfish,-1,1),
+    tau=1,
+    tauk=1,
+    tauLinf=1,
+    tauT0=1
+  )
+  
+  #-------------
+  #STEP 2:DATA
+  #-------------
+  
+  
+  # Prepare the data for JAGS
+  jags.data <- list(
+    nobs=length(data$age),
+    nfish=nfish,
+    age=data$age,
+    size=data$size,
+    ID=data$ID
+  )
+  
+  #-------------
+  #STEP 3: INDIVIDUAL BAYESIAN MODEL
+  #-------------
+  
+  sink("model.txt")
+  cat("
+    model {
+      for( i in 1 : nobs ) {
+      mu[i]<-Linf[ID[i]]*(1-exp(-k[ID[i]]*(age[i]-T0[ID[i]])))
+      #size[i] ~ dnorm(mu[i],tau)
+      shape[i]<-(mu[i]/sd)^2
+      rate[i]<-mu[i]/(sd)^2
+      size[i] ~ dgamma(shape[i],rate[i])
+      }
+      
+      #priors individual fish (ojo!the same within group variance)
+      for( i in 1 : nfish ) {
+      T0[i]~dnorm(mT0,tauT0)
+      k[i]~dnorm(mk,tauk)
+      Linf[i]~dnorm(mLinf,tauLinf)
+      }
+      
+      #prior groups
+      mT0~dunif(-2,2)
+      mk~dunif(0,1)
+      mLinf~dunif(2,10)
+      
+      #prior variances (flat prior)
+      tau ~ dgamma(0.001,0.001)
+      sd<-1/sqrt(tau)
+      tauT0 ~ dgamma(0.001,0.001)
+      tauk ~ dgamma(0.001,0.001)
+      tauLinf ~ dgamma(0.001,0.001)
+      
+}
+      ",fill = TRUE)
+sink()
+
+  
+  #-------------
+  #STEP 4: RUNNING THE MODEL AND UPDATING
+  #-------------
+  
+  # Parameters monitored
+  params =c("T0","Linf","k")
+  
+  # MCMC settings
+  nt <- 5
+  ni <- 200
+  nb <- 100
+  nc <- 3
+  
+  out <- jags(jags.data, inits, params, "model.txt", 
+              n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb)
+  
+  # Since this is calculating one curve per fish it is really time consuming and 
+  # only a few traceplots are usefull to see if the Markov Chains have converged
+  
+  #traceplot(out,varname=c("deviance"))
+  #traceplot(out,varname=c("Linf.alpha"))
+  #traceplot(out,varname=c("Linf.beta"))
+  #traceplot(out,varname=c("k.alpha"))
+  #traceplot(out,varname=c("k.beta"))
+  
+  # A rough estimation of how long the updating process will take 
+  # is provided here. 
+  pre.time <- Sys.time()
+  out <- update(out, n.iter=200,n.thin=10)
+  post.time <- Sys.time()
+  post.time-pre.time
+  
+  ### WARNING VERY TIME CONSUMING (HOURS)
+  # out <- update(out, n.iter=500000,n.thin=100)
+  
+  
+  #-------------
+  #STEP 5: RESULTS VISUALIZATION 
+  #-------------
+  
+  # To show the results and the estimates of the von Bertalanffy parameters
+  print(out)
+  # Or
+  out$BUGSoutput$summary
+  
+}
+# Plot to see the results
+
+f4 <- function(indparams,data){
+  
+  individual=indparams
+  #individual= out$BUGSoutput$summary
+  
+  #data<-read.csv(file = "data.csv",sep=";")
+  #names(data)
+  ID=NULL
+  for (i in 1: length(unique(data$fishID))){
+    temp2<-rep(i,length(data$age[which(data$fishID==unique(data$fishID)[i])]))
+    
+    ID=c(ID,temp2)
+  }
+  data$ID=ID 
+  
+  #maximum age of each fish== later will be AGE
+  AGE=NULL
+  for (i in 1: length(unique(data$fishID))){
+    temp<-max(data$age[which(data$fishID==unique(data$fishID)[i])])
+    AGE=c(AGE,temp)
+  }
   
   
   nfish=length(unique(data$fishID))
   
   # Prepare the data for JAGS
-    age=data$age
-    size=data$size
-    F=data$ID
-    AGE=Age
+  Age=data$age
+  age=data$age
+  size=data$size
+  Ind=data$ID
   max(Age)
   
- # windows(height=5.9,width=6.9, pointsize=12) #Just for the right size
+  #windows(height=5.9,width=6.9, pointsize=12) #Just for the right size
   x=10#grey scale
   ## Execute all below to see the plots
-  par(mfrow=c(round(max(Age)/2),2), mar=c(4,4,2,3))
-  seq=seq(1,max(Age)+2)
+  par(mfrow=c(round(max(age)/2),2), mar=c(4,4,2,3))
+  seq=seq(1,max(age)+2)
   
-  for (j in 2:max(Age)){
+  for (j in 2:max(age)){
     plot(age,size,type="n",xlab="", ylab="", 
          xlim= c(min(seq),max(seq)), ylim= c(0,5), las=1)
-    pre=NULL
-   
-    for (i in 1:max(Age)){
-      temp2=parse(text=paste("pepe['estimated[",i,",",j,"]',
-                             c(3,5,7)]",sep = ""))
-      pre=rbind(pre,eval(temp2))
-    }
-    polygon(c(seq(1,max(Age)),rev(seq(1,max(Age)))),c(pre[,1],rev(pre[,3])),col=gray(7/x),border=gray(7/x))
-    lines(seq(1,max(Age)),pre[,2],lwd=2, col="blue")
+    
     mtext("Von Bertalanffy Individual Model", side=3, line=0.5)
     mtext("Age (years)", side=1, line=2,cex=1)
     mtext("Size (units of size)", side=2, line=2,cex=1)
-    text(7,1, paste(j,"Year class"), cex=1.2)
+    text(4,1, paste(j,"Year class"), cex=1.2)
+    
     for(i in 1:nfish){
       if(AGE[i]==j){
-        temp=which(F==i)
+        temp=which(Ind==i)
         lines(age[temp],size[temp])
       }
     }  
---
+++ scriptpopulation.R
@@ -0,0 +1,201 @@
+f1 <- function(data){
+#------------------------------------------------
+# Fishery-dependent sampling may bias growth estimates
+
+# Supplementary material
+# Marine and Freshwater Research
+
+# Bernat Morro, Miguel Palmer, Carlos D?az-Gil, Josep Al?s, 
+# Rosario Rossell?, Amalia Grau, Inmaculada Riera-Batle, 
+# Beatriz Morales-Nin 
+#
+# Mediterranean Institute for Advanced Studies (IMEDEA UIB-CSIC)
+# Laboratory of Marine Research and Aquaculture (LIMIA)
+
+#------------------------------------------------
+# General objective: 
+
+# Objective: Comparing growth models at population and individual 
+# level (fish of diffent age) 
+# Last update: Feb 12, 2015 
+# 
+#
+# Section 1 Load the data from 296 individuals of Scorpaena porcus 
+# (age, lenght of the otolith and lenght between marks deposited 
+# in the otoliths annually)
+# Section 2 Bayesian estimation for the population von Bertalanffy 
+# model (age-at-capture, one point per fish)
+# Section 3 Bayesian estimation for the individual von Bertalanffy 
+# model (back calculated ages)
+#
+#
+#------------------------------------------------
+# Section 1: Loading libraries and data
+#------------------------------------------------
+#remove(list=ls())
+# Required libraries
+# It is necessary to download JAGS (Just Another Gibbs Sampler)
+# http://mcmc-jags.sourceforge.net/
+# And the R library to interact with it for the Bayesian approach
+library(R2jags) 
+
+#Load the file rDataScorpaenaporcus
+
+# As extra information the data has been obtained from fish 
+# obtained from commercial landings, measured and otoliths 
+# extracted, afterwards the otoliths were photographed and 
+# the position of the core, and each annual deposit mark 
+# recorded using the TPSdig2 software (version 2.17. Available at 
+# http://life.bio.sunysb.edu/ee/rohlf/software.html (Rohlf 2007). 
+# Alternatively ImageJ software can be used to obtain x,y 
+# coordinates of the points marked. In this case we directly 
+# provide with the necessary data for running both models
+
+# The code can be run line by line or all together at once. 
+
+#-------------
+# Section 2: BAYESIAN ESTIMATION OF VON BERTALANFY PARAMETERS 
+# POPULATION MODEL 1 observation per fish
+#-------------
+  ###   EL LOAD VA AQUI SI NO FUNCIONA!!!!!!! metadata <- read.csv("data.csv",sep = ";")
+#data<-read.csv(file = "data.csv",sep=";")
+names(data)
+
+Radius=NULL
+Age=NULL
+i=1
+for (i in 1: length(unique(data$fishID))){
+  temp<-max(data$size[which(data$fishID==unique(data$fishID)[i])])
+  Radius=c(Radius,temp)
+  temp2<-max(data$age[which(data$fishID==unique(data$fishID)[i])])
+  Age=c(Age,temp2)
+}
+fishID<-unique(data$fishID)
+
+
+data2<-cbind.data.frame(fishID,Radius,Age)
+
+
+
+#-------------
+#STEP 1: INITIALS
+#-------------
+
+inits <- function() list(
+  Linf=runif(1,1,10),
+  k=runif(1,0.15,0.25),
+  T0=runif(1,-1,-0.5),
+  tau=1
+)
+#-------------
+
+#-------------
+#STEP 2:DATA
+#-------------
+
+# Prepare the data for JAGS
+win.data <- list(
+  nobs=length(data2$Age),
+  age=data2$Age,
+  size=data2$Radius
+)
+
+#-------------
+#STEP 3: POBATIONAL BAYESIAN MODEL
+#-------------
+
+sink("populationmodel.txt")
+cat("
+    model {
+    for( i in 1 : nobs ) {
+    mu[i]<-Linf*(1-exp(-k*(age[i]-T0))) #this is the von Bertalanffy model
+    size[i] ~ dnorm(mu[i],tau)
+    }
+    #priors
+    T0~dunif(-2,2)
+    k~dunif(0,1)
+    Linf~dunif(0,300)
+    #prior variances
+    tau ~ dgamma(0.001,0.001)
+    sd<-1/sqrt(tau)
+    
+    #estimating size at age
+    for(i in 1 : 15) {
+    estimated[i]<-Linf*(1-exp(-k*(i-(T0))))
+    }
+    }
+    ",fill = TRUE)
+sink()
+
+#-------------
+#STEP 4: RUNNING THE MODEL AND UPDATING
+#-------------
+
+# Parameters monitored
+params =c("T0","Linf","k","sd","estimated")
+
+# MCMC settings (Markov Chain Monte Carlo)
+nt <- 5 #Number of thining
+ni <- 10000 #Number of iterations
+nb <- 100 #Number of initial burning period
+nc <- 3 #Number of Markov Chains
+
+# Launching JAGS with the poblational model
+out <- jags(win.data, inits, params, "populationmodel.txt", 
+            n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb)
+
+# The traceplots allow to see if the 3 Markov chains converge or not.
+# traceplot(out) #Keep hitting enter or escape to stop showing the plots
+
+# The MC have not converged so we update the model with more iterations
+out <- update(out, n.iter=5000,n.thin=100)## WARNING TIME CONSUMING
+
+# Ideally for assuring convergence in the estimation of the parameters
+# we need bigger number of iterations (for example see below)
+
+#out <- update(out, n.iter=500000,n.thin=100)## WARNING TIME CONSUMING
+
+
+
+#-------------
+#STEP 5: RESULTS VISUALIZATION 
+#-------------
+
+# To show the results and the estimates of the von Bertalanffy parameters
+print(out)
+# Or
+out$BUGSoutput$summary
+
+#out$BUGSoutput$summary[c(1:2,19),]
+}
+
+f3 <- function(pobparams,data){
+  
+  Radius=NULL
+  Age=NULL
+  i=1
+  for (i in 1: length(unique(data$fishID))){
+    temp<-max(data$size[which(data$fishID==unique(data$fishID)[i])])
+    Radius=c(Radius,temp)
+    temp2<-max(data$age[which(data$fishID==unique(data$fishID)[i])])
+    Age=c(Age,temp2)
+  }
+  fishID<-unique(data$fishID)
+  
+  
+  data2<-cbind.data.frame(fishID,Radius,Age)
+  attach(data2)
+# A plot to see the results
+ #X11(height=5.9,width=6.9, pointsize=12) #Just for the right size
+
+#est=out$BUGSoutput$summary[4:11,]
+est=pobparams[4:11,]
+  plot(Age,Radius, pch= 1, cex=0.5, main="Von Bertalanffy Population Model",
+     ylab= "Size (units of size)", xlab="Age (years)", xlim=c(1,8), 
+     ylim=c(0,5), las=1)
+# Median estimations of the parameters
+lines(seq(1,8),est[1:8,5],type="l",col="blue",lwd=2) 
+# Credibility intervals of the estimations
+lines(seq(1,8),est[1:8,3],col="red",lty=3)
+lines(seq(1,8),est[1:8,7],col="red",lty=3)
+}
